73450d1852bf9eba23ba6fda25a014866c0a7fdd
root <root@ip-10-0-1-153.ap-south-1.compute.internal>
2026-07-08 10:14:43 +0000

    .

--- ApiGateway/src/main/java/com/api/gateway/APIController.java
+++ ApiGateway/src/main/java/com/api/gateway/APIController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/gateway")
 public class APIController {
   
 
